435afeec837d8be77b739024c13e69e049b84523
Malith <malithwwa@gmail.com>
2026-02-17 12:51:30 +0530

    implement coderabbit suggested fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -7,27 +7,87 @@ function sendJson(socket, payload) {
 
 function broadcast(wss, payload) {
     for ( const client of wss.clients ) {
-        if (client.readyState !== WebSocket.OPEN) return;
+        if (client.readyState !== WebSocket.OPEN) continue;
         client.send(JSON.stringify(payload));
     }
 }
 
 export function attachWebsocket(server) {
-    const wss = new WebSocketServer({
-        server,
-        path: '/ws',
-        maxPayload: 1024 * 1024
-    });
+    const wss = new WebSocketServer({ noServer: true, path: '/ws', maxPayload: 1024 * 1024 });
 
-    wss.on ('connection', socket => {
+    // server.on('upgrade', async (req, socket, head) => {
+    //     const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+    //
+    //     if (pathname !== '/ws') {
+    //         return;
+    //     }
+    //
+    //     if (wsArcjet) {
+    //         try {
+    //             const decision = await wsArcjet.protect(req);
+    //
+    //             if (decision.isDenied()) {
+    //                 if (decision.reason.isRateLimit()) {
+    //                     socket.write('HTTP/1.1 429 Too Many Requests\r\n\r\n');
+    //                 } else {
+    //                     socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
+    //                 }
+    //                 socket.destroy();
+    //                 return;
+    //             }
+    //         } catch (e) {
+    //             console.error('WS upgrade protection error', e);
+    //             socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
+    //             socket.destroy();
+    //             return;
+    //         }
+    //     }
+    //
+    //     wss.handleUpgrade(req, socket, head, (ws) => {
+    //         wss.emit('connection', ws, req);
+    //     });
+    // });
+
+    wss.on('connection', (socket) => {
+        socket.isAlive = true;
+        socket.on('pong', () => { socket.isAlive = true; });
+
+        // socket.subscriptions = new Set();
+
         sendJson(socket, { type: 'welcome' });
+
+        // socket.on('message', (data) => {
+        //     handleMessage(socket, data);
+        // });
+        //
+        // socket.on('error', () => {
+        //     socket.terminate();
+        // });
+        //
+        // socket.on('close', () => {
+        //     cleanupSubscriptions(socket);
+        // })
 
         socket.on('error', console.error);
     });
 
-    function broadcastMatchCreated(match){
-        broadcast(wss, { type: 'match_created', data: match });
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+
+            ws.isAlive = false;
+            ws.ping();
+        })}, 30000);
+
+    wss.on('close', () => clearInterval(interval));
+
+    function broadcastMatchCreated(match) {
+        broadcastToAll(wss, { type: 'match_created', data: match });
     }
 
-    return {broadcastMatchCreated}
+    // function broadcastCommentary(matchId, comment) {
+    //     broadcastToMatch(matchId, { type: 'commentary', data: comment });
+    // }
+
+    return { broadcastMatchCreated };
 }
